9ed8fd9983afa725b2dc68c243b6da2028011870
Stavan Bhatt <stavan.b@simformsolutions.com>
2022-04-12 17:41:30 +0530

    Implemented Capture in RN Vision Camera, and display captured image

--- app/modules/HomeScreen/Home.js
+++ app/modules/HomeScreen/Home.js
@@ -1,15 +1,62 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import images from '../../assets/images/images';
 import styles from './styles';
 
 const Home = () => {
+  const [image, setImage] = useState(null);
+  const [isCaptured, setCaptured] = useState(false);
+  const devices = useCameraDevices();
+  const device = devices.back;
+  const camera = useRef(null);
+  const takePhoto = () => {
+    camera.current
+      .takeSnapshot({
+        quality: 85,
+        skipMetadata: true,
+      })
+      .then(res => {
+        setImage(res.path);
+        setCaptured(true);
+      });
+  };
+
+  if (device == null) {
+    return <ActivityIndicator />;
+  }
   return (
-    <View style={styles.screen}>
-      <Text style={styles.textStyle}>
-        Welcome To React Native Vision Camera Library Demo
-      </Text>
-      <Text style={styles.textStyle}>You can begin you work here.</Text>
-    </View>
+    <>
+      <Camera
+        ref={camera}
+        style={styles.screen}
+        device={device}
+        isActive={true}
+        photo={true}
+      />
+      {!isCaptured ? (
+        <View style={styles.captureContainer}>
+          <TouchableOpacity style={styles.circleRing} onPress={takePhoto} />
+        </View>
+      ) : (
+        <ImageBackground
+          style={styles.displayImage}
+          source={{ uri: `file://${image}` }}>
+          <TouchableOpacity
+            onPress={() => {
+              setCaptured(false);
+            }}>
+            <Image source={images.cancel} style={styles.cancelButton} />
+          </TouchableOpacity>
+        </ImageBackground>
+      )}
+    </>
   );
 };
 
--- app/modules/HomeScreen/styles.js
+++ app/modules/HomeScreen/styles.js
@@ -1,16 +1,34 @@
 import { StyleSheet } from 'react-native';
+import { Metrics } from '../../theme';
 
+const { verticalScale, horizontalScale, moderateScale } = Metrics;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  textStyle: {
-    fontSize: 18,
-    margin: 5,
-    marginVertical: 5,
-    textAlign: 'center',
+  captureContainer: {
+    flex: 1,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: '100%',
+    width: '100%',
+  },
+  circleRing: {
+    borderWidth: 5,
+    padding: moderateScale(30),
+    marginBottom: verticalScale(40),
+    borderRadius: moderateScale(80),
+    borderColor: 'white',
+  },
+  displayImage: {
+    height: '100%',
+  },
+  cancelButton: {
+    height: horizontalScale(40),
+    width: verticalScale(40),
+    marginLeft: horizontalScale(15),
+    marginTop: verticalScale(15),
   },
 });
 
